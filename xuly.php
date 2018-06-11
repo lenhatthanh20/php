62e90e23bfbd58aae2e167c568df377411afc541
thanh <lenhatthanh20@gmail.com>
2018-06-11 19:19:26 +0700

    addxulyphp

---
+++ xuly.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: NhatThanh
+ * Date: 11/06/2018
+ * Time: 7:04 PM
+ */
+$bienA = 'Bien A';
+$bienB = $_GET;
+$bienC = $_POST;
+
+print $bienA;
+print "<br>";
+print_r($bienB);
+
+print "<br>";
+print_r($bienC);
